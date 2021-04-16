508bb36e814c4320c61fe8970ac425b64cc60051
motezgh <motez.gharbi@hotmail.com>
2021-04-16 23:21:02 +0100

    add archive class

---
+++ src/app/model/arch-1004901.spec.ts
@@ -0,0 +1,7 @@
+import { Arch1004901 } from './arch-1004901';
+
+describe('Arch1004901', () => {
+  it('should create an instance', () => {
+    expect(new Arch1004901()).toBeTruthy();
+  });
+});
---
+++ src/app/model/arch-1004901.ts
@@ -0,0 +1,74 @@
+export class Arch1004901 {
+    
+	date: Date ;
+	
+	id_device: number ;
+	
+	speed:number ;
+	
+	fuel:number ;
+	
+	temp:number ;
+	
+	X:number ;
+	
+	Y:number ;
+	
+	Z:number ;
+	
+	ignition:number ;
+	
+	rpm:number ;
+	
+	fuel_rate:number ;
+	
+	tfu:number ;
+	
+	odo:number ;
+	
+	satInView:number ;
+	
+	signal:number ;
+	
+	heading:number ;
+	
+	charger:number ;
+	
+	latitude:number ;
+	
+	longitude:number ;
+	
+	stat:number ;
+	
+	tram_id:number ;
+	
+	validity:number ;
+	
+	temp_engine:number ;
+	
+	accum_odo:number ;
+	
+	do1:number ;
+	
+	do2:number ;
+	
+	do3 :number;
+	
+	do4 :number;
+	
+	di1 :number;
+	
+	di2 :number;
+	
+	di3:number ;
+	
+	di4:number ;
+	
+	an1 :number;
+	
+	an2:number ;
+	
+	an3:number ;
+	
+	an4:number ;
+}
