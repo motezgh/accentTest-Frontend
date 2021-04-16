861626cd4eebe06eaced17d804c296f3d2fc5260
motezgh <motez.gharbi@hotmail.com>
2021-04-16 23:51:41 +0100

    create function to get data from API in service

--- src/app/services/archive.service.ts
+++ src/app/services/archive.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Arch1004901 } from "../model/arch-1004901";
+import { host } from "../domain/domain";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArchiveService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getData():Observable<Arch1004901[]>{
+    return this.http.get<getResponseArchive>(host+'/archive')
+    .pipe(map(response=>response._embedded.arch1004901s))
+  }
+
+}
+
+interface getResponseArchive{
+  _embedded:{
+    arch1004901s:Arch1004901[];
+  }
 }
